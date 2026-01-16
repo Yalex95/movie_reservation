26da26edcfcef2d2066b91c99d0f703159bbadca
yerisaguilar <yerisaguilarg@gmail.com>
2026-01-16 11:18:34 -0800

    added tailwid and daisy-ui and config zod .env validation

---
+++ app/app.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+  <button class="btn btn-secondary"> <Icon name="tabler:map" /> Secondarysdasd</button>
+   
+  </div>
+</template>
